7f5a213ca2064df78549dfab40dbbb51b476c927
pavithrachamala <56586609+pavithrachamala@users.noreply.github.com>
2020-04-24 15:05:21 +0530

    Add files via upload

---
+++ EmpDetailsApplication/src/main/java/com/employeedetails/service/EmployeeService.java
@@ -0,0 +1,10 @@
+package com.employeedetails.service;
+import java.util.List;
+
+import com.employeedetails.entities.Employee;
+public interface EmployeeService {
+	public List<Employee> findAll();
+	public List<Employee> salaryLimit(Double salary);
+	public Employee findByEmpId(Long id);
+	public void createemployee(Employee employee);
+}
